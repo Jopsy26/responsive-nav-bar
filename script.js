a30d3289c83abffdd29b788b476c7ddba7ce2f2e
Jopsy <114136727+Jopsy26@users.noreply.github.com>
2023-06-30 20:47:43 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const showMenu = () => {
+  document.getElementById("links-container").style.left = 0;
+};
+
+const closeMenu = () => {
+  document.getElementById("links-container").style.left = "-50%";
+};
